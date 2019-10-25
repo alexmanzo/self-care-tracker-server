948d99be2df1c8d4f2efa2f5221f6b9834c338a7
Alex Manzo <manzoalex17@gmail.com>
2019-10-25 15:33:33 -0400

    Update DATABASE_URL config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 exports.PORT = process.env.PORT || 3000;
-exports.DATABASE_URL = process.env.DATABASE_URL ? process.env.DATABASE_URL
-    : 'mongodb://' +
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://' +
         process.env.MONGO_LOCAL_USERNAME + ':' +
         process.env.MONGO_LOCAL_PASSWORD + '@' +
         process.env.MONGO_LOCAL_HOST + '/' +
