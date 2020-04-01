e322b0d51110de22a014bae4d110af9ec0c0bf18
Alex Manzo <manzoalex17@gmail.com>
2020-04-01 18:07:08 -0400

    Update config file.

--- config.js
+++ config.js
@@ -5,6 +5,3 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://' +
         process.env.MONGO_LOCAL_PASSWORD + '@' +
         process.env.MONGO_LOCAL_HOST + '/' +
         process.env.MONGO_LOCAL_DATABASE;
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-self-care-tracker';
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
